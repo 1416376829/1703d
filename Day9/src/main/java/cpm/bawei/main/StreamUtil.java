e03b173aa259fff7f4c3063e832c63ff5ca03434
吕铮 <1416376829@qq.com>
2019-07-16 08:49:44 +0800

    day9提交

---
+++ Day9/src/main/java/cpm/bawei/main/StreamUtil.java
@@ -0,0 +1,35 @@
+package cpm.bawei.main;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class StreamUtil {
+	public static void closeAll() {
+		
+		}
+   public static void copy(InputStream src,OutputStream out,boolean isCloseInputStream,boolean isCloseOutputStream)throws IOException{
+	   
+   }
+   public static String readTextFile(InputStream src)throws IOException{
+	return null;
+	   
+   }
+   public static String readTextFile(File txtFile)throws FileNotFoundException,IOException{
+	return null;  
+   }
+   public static String readTextFile1(File txtFile)throws FileNotFoundException,IOException{
+	return null;
+   }
+   public static String readStringFromSystemIn(String message){
+	return message;
+	   
+   }
+   public static int readIntFromSystemIn(String message){
+	return 0;
+	   
+   }
+   
+}
---
+++ Day9/src/test/java/com/bawei/test/DayTest.java
@@ -0,0 +1,67 @@
+package com.bawei.test;
+
+import org.junit.Test;
+
+public class DayTest {
+@Test
+public void fun1test(){
+	try {
+		copy();
+	} catch (Exception e) {
+      e.printStackTrace();
+	}
+}
+private void copy() {
+	// TODO Auto-generated method stub
+	
+}
+@Test
+public void fun2test(){
+	try {
+		readTextFile();
+	} catch (Exception e) {
+      e.printStackTrace();
+	}
+}
+private void readTextFile() {
+	// TODO Auto-generated method stub
+	
+}
+@Test
+public void fun3test(){
+	try {
+		readTextFile();
+	} catch (Exception e) {
+      e.printStackTrace();
+	}
+}
+@Test
+public void fun4test(){
+	try {
+		readTextFile();
+	} catch (Exception e) {
+      e.printStackTrace();
+	}
+}
+@Test
+public void fun5test(){
+	try {
+		readStringFromSystemIn();
+	} catch (Exception e) {
+      e.printStackTrace();
+	}
+}
+private void readStringFromSystemIn() {
+	// TODO Auto-generated method stub
+	
+}
+@Test
+public void fun6test(){
+	try {
+		readStringFromSystemIn();
+	} catch (Exception e) {
+      e.printStackTrace();
+	}
+}
+
+}
