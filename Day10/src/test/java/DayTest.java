97fb6bc511f1a91f7776489c91ee467151995210
吕铮 <1416376829@qq.com>
2019-07-17 08:46:47 +0800

    12

---
+++ Day10/src/test/java/DayTest.java
@@ -0,0 +1,39 @@
+import javax.xml.ws.WebServiceException;
+
+import org.junit.Test;
+
+
+public class DayTest {
+	@Test
+public void fun1test(){
+	try {
+	Class<? extends DayTest> class1 = getClass();
+	} catch (WebServiceException e) {
+		e.printStackTrace();
+	}
+}
+	@Test
+public void fun2test(){
+	try {
+	Class<? extends DayTest> class1 = getClass();
+	} catch (WebServiceException e) {
+		e.printStackTrace();
+	}
+}
+	@Test
+public void fun3test(){
+	try {
+	Class<? extends DayTest> class1 = getClass();
+	} catch (WebServiceException e) {
+		e.printStackTrace();
+	}
+}
+	@Test
+public void fun4test(){
+	try {
+	Class<? extends DayTest> class1 = getClass();
+	} catch (WebServiceException e) {
+		e.printStackTrace();
+	}
+}
+}
